05cbb2d7a199234475c7bf57c9d2564ac27793cc
Ankita Patra <debjaniankita123@gmail.com>
2026-04-03 17:36:48 +0530

    added streamlit app

---
+++ app/app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import numpy as np
+from src.utils import load_object
+
+model = load_object("models/kmeans_model.pkl")
+scaler = load_object("models/scaler.pkl")
+
+st.title("Customer Segmentation App")
+
+income = st.number_input("Income")
+spending = st.number_input("Total Spending")
+age = st.number_input("Age")
+children = st.number_input("Total Children")
+
+if st.button("Predict"):
+    data = np.array([[income, spending, age, children]])
+    data_scaled = scaler.transform(data)
+
+    cluster = model.predict(data_scaled)[0]
+
+    st.success(f"Customer belongs to Cluster {cluster}")
